4a6551afea2ef151553c8347b9afefbce7d48bda
Joel Lawton <joellawton65@gmail.com>
2019-10-10 22:26:53 +0100

    Search

--- src/App.vue
+++ src/App.vue
@@ -11,23 +11,68 @@
 
 <template>
   <div id="app">
-    <div v-for="(city, index) in $store.state.cities.results" :key="index">
-      {{city.name}}
+    <h1>Compare your Air</h1>
+    <p>Compare the air quality between cities in the UK.</p>
+    <p>Select cities to compare using the search tool below.</p>
+    <div class="drop-down">
+      <input type="text" placeholder="Enter city name..." 
+      v-on:focus="focusDropDown"
+      v-on:blur="blurDropDown" 
+      v-model="search"/>
+      <div v-if="openDropDown === true">
+        <a 
+        v-for="(city, index) in filterCities" :key="index"
+        v-on:click="search = city.name">{{city.name}}</a>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-import { mapActions } from 'vuex';
+import { mapActions, mapState } from 'vuex';
+import openqaService from './services/openqa.js';
+import Multiselect from 'vue-multiselect';
 
 export default {
   name: 'app',
-  components: {},
+  components: {
+    Multiselect
+  },
+  data(){
+    return {
+      search: '',
+      openDropDown: false,
+      cityResults: []
+    }
+  },
   created: function(){
     this.getCities();
   },
   methods: {
-    ...mapActions(['getCities'])
+    ...mapActions(['getCities']),
+    getCity(cityName){
+      openqaService.getCity(cityName).then(resp => {
+        console.log(resp.data);
+      }).catch(() => {
+
+      });
+    },
+    focusDropDown(){
+      this.openDropDown = true;
+    },
+    blurDropDown(){
+      if(search.length < 0){
+        this.openDropDown = false;
+      }
+    }
   },
+  computed:{
+    ...mapState(['cities']),
+    filterCities(){
+      return this.cities.results.filter(x => {
+        return x.name.toLowerCase().includes(this.search.toLowerCase());
+      });
+    }
+  }
 }
 </script>
